cc170503cc6e3441c85a95474d7d6dc21b54db8a
Sehunwy <625192678@qq.com>
2018-09-18 21:03:14 +0800

    界面设计

---
+++ agritainment/front_end/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require("fs");
+var bodyParser = require('body-parser');
+//handle request entity too large
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
+//设置静态文件
+app.use(express.static('public'));
+//设置html引擎
+app.set('view engine', 'html');
+//设置视图地址
+app.set('views', path.join(__dirname, '/views'));
+//设置html引擎
+app.engine('html', require('ejs').__express);
+
+var urlencodedParser=bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+
+//登录注册
+var loginController = require('./controllers/LoginController');
+app.get('/login', loginController.login);
+
+//首页
+var indexController = require('./controllers/IndexController');
+app.get('/index', indexController.index);
+
+
+
+app.listen(8888);
